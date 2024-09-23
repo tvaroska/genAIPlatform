cacf39ac1f33362c9ab1a11d71a35478acb4b2f9
Boris Tvaroska <boris@tvaroska.sk>
2024-09-23 14:58:35 +0000

    bug: make gke private

--- infra/platform.tf
+++ infra/platform.tf
@@ -9,6 +9,12 @@ resource "google_container_cluster" "primary" {
   enable_autopilot = true
   deletion_protection = false
 
+  # Private cluster configuration
+  private_cluster_config {
+    enable_private_nodes    = true
+    enable_private_endpoint = true
+#    master_ipv4_cidr_block  = "172.16.0.0/28"  # Specify a /28 CIDR block for the master
+  }
 }
 
 # Cloud SQL Postgres instance in services subnet
